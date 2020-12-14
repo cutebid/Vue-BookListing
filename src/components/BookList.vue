a2c38423638ec2cfa7c41bdee5ef9458face63a1
cutebid <67562053+cutebid@users.noreply.github.com>
2020-12-14 14:58:45 +0100

    Completed module

---
+++ src/components/BookList.vue
@@ -0,0 +1,15 @@
+<template>
+     <div>
+        <h1>{{title}}</h1>
+     </div>
+</template>
+<script>
+export default{
+    name : BookList
+    data : function(){ 
+      title: "All Books" }
+      }
+</script
+<style>
+     h1, h2 {font-weight : normal}
+</style
